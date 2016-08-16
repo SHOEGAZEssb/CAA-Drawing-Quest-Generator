5a55192eb360717a22a7c9026f939f90453326f8
Tim Stadler <timstadler905@googlemail.com>
2016-08-16 18:49:04 +0200

    Category stuff

---
+++ CAA-Drawing-Quest-Generator/Trait Categories/ElementCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAA_Drawing_Quest_Generator.Trait_Categories
+{
+  class ElementCategory
+  {
+  }
+}
